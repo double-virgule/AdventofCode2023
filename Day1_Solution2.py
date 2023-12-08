f5cfc3ae010d1c221795be841dafca91b15c97c6
Mark Mansur <16672129+double-virgule@users.noreply.github.com>
2023-12-08 13:41:27 -0500

    Update Day1_Solution2.py

--- Day1_Solution2.py
+++ Day1_Solution2.py
@@ -1,18 +1,20 @@
-# Add spelled letters
+import re
 
-file_path = "C:/Data/AdventofCode2023/Day1/input.txt"
+file_path = "input"
 with open(file_path, 'r') as file:
     data = file.readlines()
 
 total_count = 0
 
-words = ["one","two","three","four","five","six","seven","eight","nine"]
 
 for i in data: 
     digits = []
-    if words[0] in i: 
-        print(words[0])
-    for j in i:
+
+    i_replaced = i.replace("one","one1one").replace("two","two2two").replace("three","three3three").replace("four","four4four").replace("five","five5five").replace("six","six6six").replace("seven","seven7seven").replace("eight","eight8eight").replace("nine","nine9nine")
+
+    print(i_replaced)
+
+    for j in i_replaced:
         if j.isdigit(): 
             digits += j
     if len(digits) == 1: 
@@ -22,5 +24,3 @@ for i in data:
     total_count += int(output) 
 
 print(total_count)
-    
-    
